c03a450138dd82c9b12fec37f80e901dc7f09567
Kim Sanghyeok <skyee23@gmail.com>
2018-05-23 15:18:46 +0900

    로그인 폼 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
+const postAPI = axios.create({});
+
 const rootEl = document.querySelector('.root');
 const templates = {
   postList: document.querySelector('#post-list').content,
   postItem: document.querySelector('#post-item').content,
-  postContent: document.querySelector('#post-content').content
+  postContent: document.querySelector('#post-content').content,
+  login: document.querySelector('#login').content
 }
 
 function render(fragment){
@@ -13,9 +16,13 @@ function render(fragment){
 }
 
 async function indexPage(){
-   const res = await axios.get('http://localhost:3000/posts');
+   const res = await postAPI.get('http://localhost:3000/posts');
    const listFragment = document.importNode(templates.postList, true);
-    
+  
+   listFragment.querySelector('.post-list__login-btn').addEventListener('click', e=>{
+    loginPage();
+   })
+
    res.data.forEach(post => {
      const fragment = document.importNode(templates.postItem, true);
      const pEl = fragment.querySelector('.post-item__title');
@@ -29,11 +36,29 @@ async function indexPage(){
 }
 
 async function postContentPage(postId){
-  const res = await axios.get(`http://localhost:3000/posts/${postId}`);
+  const res = await postAPI.get(`http://localhost:3000/posts/${postId}`);
   const fragment = document.importNode(templates.postContent, true);
   fragment.querySelector('.post-content__title').textContent = res.data.title;
   fragment.querySelector('.post-content__body').textContent = res.data.body;
   fragment.querySelector('.post-content__back-btn').addEventListener('click', e=>{
+    indexPage();
+  })
+  render(fragment);
+}
+
+async function loginPage(){
+  const fragment = document.importNode(templates.login, true);
+  const formEl = fragment.querySelector('.login__form');
+  formEl.addEventListener('submit', async e=>{
+    // e.target.elements.username.value === fragment.querySelector('.login__username');
+    const payload = {
+      username: e.target.elements.username.value,
+      password: e.target.elements.password.value
+    };
+    e.preventDefault();
+    const res = await postAPI.post('http://localhost:3000/users/login',payload);
+    localStorage.setItem('token',res.data.token); 
+    postAPI.defaults.headers['Authorization'] = res.data.token;
     indexPage();
   })
   render(fragment);
