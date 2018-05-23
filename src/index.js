5074faaf8dd72c4174a7e63f272b579d926a0666
Kim Sanghyeok <skyee23@gmail.com>
2018-05-23 12:32:43 +0900

    템플릿을 활용한 코드수정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
 
-axios.get('http://localhost:3000/posts').then(res => {
-  res.data.forEach(post => {
-    const fragment = document.importNode(document.querySelector('#post-item').content, true);
-    const pEl = fragment.querySelector('.post-item__title');
-    pEl.textContent = post.title;
-    document.querySelector('.post-list').appendChild(fragment);
-  })
-})
+const rootEl = document.querySelector('.root');
+const templates = {
+  postList: document.querySelector('#post-list').content,
+  postItem: document.querySelector('#post-item').content
+}
+
+async function indexPage(){
+   const res = await axios.get('http://localhost:3000/posts');
+   const listFragment = document.importNode(templates.postList, true);
+    
+   res.data.forEach(post => {
+     const fragment = document.importNode(templates.postItem, true);
+     const pEl = fragment.querySelector('.post-item__title');
+     pEl.textContent = post.title;
+     listFragment.querySelector('.post-list').appendChild(fragment);
+   })
+ 
+   rootEl.appendChild(listFragment);
+}
+
+indexPage();
