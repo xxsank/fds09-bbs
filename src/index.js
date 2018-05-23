ca390172fdbe4285230f8c75f980f8c4fb332f92
Kim Sanghyeok <skyee23@gmail.com>
2018-05-23 11:53:13 +0900

    json server setting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import './index.scss';
+import axios from 'axios';
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+axios.get('http://localhost:3000/posts').then(res => {
+  res.data.forEach(post => {
+    const fragment = document.importNode(document.querySelector('#post-item').content, true);
+    const pEl = fragment.querySelector('.post-item__title');
+    pEl.textContent = post.title;
+    document.querySelector('.post-list').appendChild(fragment);
+  })
+})
