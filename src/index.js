81680122ca94107732b8bad83e2fcdfddb5835df
Kim Sanghyeok <skyee23@gmail.com>
2018-05-23 13:06:21 +0900

    게시글 페이지 추가

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import axios from 'axios';
 const rootEl = document.querySelector('.root');
 const templates = {
   postList: document.querySelector('#post-list').content,
-  postItem: document.querySelector('#post-item').content
+  postItem: document.querySelector('#post-item').content,
+  postContent: document.querySelector('#post-content').content
+}
+
+function render(fragment){
+  rootEl.textContent = "";  
+  rootEl.appendChild(fragment);
 }
 
 async function indexPage(){
@@ -14,10 +20,25 @@ async function indexPage(){
      const fragment = document.importNode(templates.postItem, true);
      const pEl = fragment.querySelector('.post-item__title');
      pEl.textContent = post.title;
+     pEl.addEventListener('click', e=>{
+       postContentPage(post.id);
+     })
      listFragment.querySelector('.post-list').appendChild(fragment);
    })
- 
-   rootEl.appendChild(listFragment);
+   render(listFragment);
+}
+
+async function postContentPage(postId){
+  const res = await axios.get(`http://localhost:3000/posts/${postId}`);
+  const fragment = document.importNode(templates.postContent, true);
+  fragment.querySelector('.post-content__title').textContent = res.data.title;
+  fragment.querySelector('.post-content__body').textContent = res.data.body;
+  fragment.querySelector('.post-content__back-btn').addEventListener('click', e=>{
+    indexPage();
+  })
+  render(fragment);
 }
 
 indexPage();
+
+// postContentPage(1);
